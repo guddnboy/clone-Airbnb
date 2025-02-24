263a391d6ddf4c8965581e80a626f9905d839357
guddnboy <hung3018@naver.com>
2025-02-24 16:06:34 +0900

    feat: 숙소, 체험 버튼 스타일 추가

--- src/components/Top/ChooseTypeButton.js
+++ src/components/Top/ChooseTypeButton.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { use, useEffect } from 'react';
+import { useState } from 'react';
 
 function ChooseTypeButton() {
+  const [isStay, setIsStay] = useState(true);
+  const [isTry, setIsTry] = useState(false);
+
+  const selectStay = () => {
+    if (!isStay) {
+      setIsStay(true);
+      setIsTry(false);
+    }
+  };
+
+  const selectTry = () => {
+    if (!isTry) {
+      setIsTry(true);
+      setIsStay(false);
+    }
+  };
+
   return (
     <div className='flex items-center justify-center w-64'>
       <div className='flex w-14 h-10 justify-center'>
-        <button className='w-full h-full hover:bg-gray-100 hover:rounded-2xl'>
+        <button
+          className={`w-full h-full hover:bg-gray-100 hover:rounded-2xl ${isTry ? 'text-gray-500' : 'text-black'}`}
+          onClick={() => {
+            selectStay();
+          }}
+        >
           숙소
         </button>
       </div>
       <div className='flex w-14 h-10 justify-center'>
-        <button className='w-full h-full hover:bg-gray-100 hover:rounded-2xl'>
+        <button
+          className={`w-full h-full hover:bg-gray-100 hover:rounded-2xl ${isTry ? 'text-black' : 'text-gray-500'}`}
+          onClick={() => {
+            selectTry();
+          }}
+        >
           체험
         </button>
       </div>
