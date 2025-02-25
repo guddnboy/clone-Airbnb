7dc3a7e2b403f1d3f5c792b125d9c477ca85255b
guddnboy <hung3018@naver.com>
2025-02-25 17:12:23 +0900

    feat: 카테고리 아이콘 컴포넌트 렌더링

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import CategoryIcon from './CategoryIcon';
 
 function CategoryFilter() {
-  return <div>카테고리 필터</div>;
+  const [categories, setCategories] = useState([]);
+
+  function getCategoryData() {
+    try {
+      fetch('/data/categoryData.json')
+        .then((response) => response.json())
+        .then((data) => {
+          setCategories(data.categories);
+        });
+    } catch (error) {
+      console.error('데이터 로딩 실패', error);
+    }
+  }
+  useEffect(() => {
+    getCategoryData();
+  }, []);
+
+  return (
+    <div className='flex items-center overflow-y-hidden h-16 px-12 gap-4'>
+      {categories.map((category, idx) => (
+        <CategoryIcon key={idx} img={category.img} name={category.name} />
+      ))}
+    </div>
+  );
 }
 export default CategoryFilter;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../components/Top/Header';
 import Footer from '../components/Footer';
 import LodgingList from '../components/LodgingList';
+import CategoryFilter from '../components/CategoryFilter';
 
 function HomePage() {
   return (
     <>
       <Header />
+      <CategoryFilter />
       <LodgingList />
       <Footer />
     </>
