72170e005f72295c77d4247f5e4d2c1e22d043dd
guddnboy <hung3018@naver.com>
2025-02-25 14:48:15 +0900

    chore: 검색 버튼 이름에 맞게 파일 수정

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,19 +1,6 @@
 import React from 'react';
 
 function CategoryFilter() {
-  return (
-    <div className='flex justify-center w-full h-16'>
-      <div className='flex w-[45rem] border rounded-full border-gray-300 bg-gray-200'>
-        <div className='flex flex-col w-[15rem] pl-4 justify-center rounded-full border bg-white shadow-lg'>
-          <button className='w-14 flex flex-col text-xs pr-4'>여행지</button>
-          <input
-            type='text'
-            placeholder='여행지 검색'
-            className='w-fit border-none text-sm outline-none pl-1 bg-inherit'
-          />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>카테고리 필터</div>;
 }
 export default CategoryFilter;
