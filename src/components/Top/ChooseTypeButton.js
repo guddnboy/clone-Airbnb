b8a2616b3d2fa348b47b49458ca413b1b8cb2fb7
guddnboy <hung3018@naver.com>
2025-02-27 14:10:14 +0900

    chore: 할 일과 살 것으로 이름 변경

--- src/components/Top/ChooseTypeButton.js
+++ src/components/Top/ChooseTypeButton.js
@@ -2,43 +2,43 @@ import React from 'react';
 import { useState } from 'react';
 
 function ChooseTypeButton() {
-  const [isStay, setIsStay] = useState(true);
-  const [isTry, setIsTry] = useState(false);
+  const [isTodo, setisTodo] = useState(true);
+  const [isTobuy, setisTobuy] = useState(false);
 
   const selectStay = () => {
-    if (!isStay) {
-      setIsStay(true);
-      setIsTry(false);
+    if (!isTodo) {
+      setisTodo(true);
+      setisTobuy(false);
     }
   };
 
   const selectTry = () => {
-    if (!isTry) {
-      setIsTry(true);
-      setIsStay(false);
+    if (!isTobuy) {
+      setisTobuy(true);
+      setisTodo(false);
     }
   };
 
   return (
     <div className='flex items-center justify-center w-64 pl-40'>
-      <div className='flex w-14 h-10 justify-center'>
+      <div className='flex w-24 h-10 justify-center'>
         <button
-          className={`w-full h-full hover:bg-gray-100 hover:rounded-2xl ${isTry ? 'text-gray-500' : 'text-black'}`}
+          className={`w-20 h-full hover:bg-gray-100 hover:rounded-2xl ${isTobuy ? 'text-gray-500' : 'text-black'}`}
           onClick={() => {
             selectStay();
           }}
         >
-          숙소
+          TODO
         </button>
       </div>
-      <div className='flex w-14 h-10 justify-center'>
+      <div className='flex w-24 h-10 justify-center'>
         <button
-          className={`w-full h-full hover:bg-gray-100 hover:rounded-2xl ${isTry ? 'text-black' : 'text-gray-500'}`}
+          className={`w-20 h-full hover:bg-gray-100 hover:rounded-2xl ${isTobuy ? 'text-black' : 'text-gray-500'}`}
           onClick={() => {
             selectTry();
           }}
         >
-          체험
+          TOBUY
         </button>
       </div>
     </div>
